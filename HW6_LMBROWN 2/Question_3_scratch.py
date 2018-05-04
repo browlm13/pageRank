8b94b2a8721e799cb35dd74f25965a70ebf97665
LJ Brown <browlm13@gmail.com>
2018-05-03 22:16:15 -0500

    Add files via upload

--- HW6_LMBROWN 2/Question_3_scratch.py
+++ HW6_LMBROWN 2/Question_3_scratch.py
@@ -1,6 +1,7 @@
 """
 Question 3
 naive bayes
+
 """
 from functools import reduce
 
@@ -30,32 +31,44 @@ probabilities = {
 			'f3' : 0.005,
 			'f4' : 0.005,
 			'f5' : 0.01
-		}
+		},
+	'f1' : 0.41,
+	'f2' : 0.16,
+	'f3' : 0.106,
+	'f4' : 0.206,
+	'f5' : 0.023
 }
 
 
-def propto_prob_class_given_features(class_name, features_list):
-	""" c_i * f_0 * ... * f_n, for each feature probability in class column for f_i in features list"""
 
+def prob_class_given_features(class_name, features_list):
 	global probabilities
 
 	cp = [probabilities[class_name]['P']]
+	fps = [probabilities[f] for f in features_list]
 	fgcs = [probabilities[class_name][f] for f in features_list]
 
-	return reduce(lambda x, y: x*y, cp + fgcs)
+	numerator =  reduce(lambda x, y: x*y, cp + fgcs)
+	denominator = reduce(lambda x, y: x*y, fps)
+
+	return numerator/denominator
 
 
 def find_class(features_list):
 	global class_names
 
+	print("for features %s" % str(features_list))
+
+
 	c_pcs = []
 	for c in class_names:
-		pc = propto_prob_class_given_features(c, features_list)
+		pc = prob_class_given_features(c, features_list)
 		c_pcs.append((pc, c))
+		print("probability of class %s is %s" % (c, pc))
 
 	# find maximum prob
 	c_pcs.sort()
-	print('\nFor features %s,\nlikley class is %s' % (str(features_list), c_pcs[-1][1]))
+	print('likley class for given features is %s' % c_pcs[-1][1])
 
 
 find_class(['f1', 'f2', 'f3'])
